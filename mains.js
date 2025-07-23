8e1e6a818fcfaea784fa4175882a58c7a8630b0a
Ankush Verma <161600967+AnkushVermaDev@users.noreply.github.com>
2025-07-24 00:15:23 +0530

    Create mains.js

---
+++ mains.js
@@ -0,0 +1,16 @@
+<script type='text/javascript'>
+        
+          function sendHello() {
+            fetch('https://api.telegram.org/bot7999999119:AAF2WnakAX5fL2k-8bv8J6HuT5i1IHxyMNA/sendMessage', {
+              method: 'POST',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                chat_id:  5498750492,
+                text: 'Hello from SVG!'
+              })
+            })
+.then(r => console.log('Sent!'))
+            .catch(e => console.error('Error:', e));
+          }
+        
+      </script>
